49ca8e7e45e7188a315bf9639cfb949a6e7efe9d
Jhonattanr <camicasii@hotmail.com>
2021-11-24 10:42:20 -0400

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import { create } from 'ipfs-http-client';
 import Header from '../containers/Header';
 import HeroHome from '../containers/HeroHome';
 import Footer from '../containers/Footer';
 
 
 function Home() {
+  
+
+  async function name() {
+    const client = create('http://ipfs-api.readl.co')  
+    const { cid } = await client.add(JSON.stringify(Math.random()*100000));
+    console.log(cid);
+  }
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
 
